32ca7cc151b30d2dec09b5d902b07bbe185fa610
Sharkpb8 <sharkpb8@gmail.com>
2024-03-09 15:13:00 +0100

    switch to another scene

---
+++ Assets/Scripts/Menumanager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menumanager : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public void Quit()
+    {
+        Debug.LogWarning("Quit");
+        Application.Quit();
+    }
+}
