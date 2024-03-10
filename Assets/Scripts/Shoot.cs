c56a6f21e6744e199fe7091c2d1a450e22587fbd
Sharkpb8 <sharkpb8@gmail.com>
2024-03-10 14:23:39 +0100

    bullet delay added

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -6,19 +6,24 @@ public class Shoot : MonoBehaviour
 {
     public GameObject bullet;
     public Transform shootPoint;
+    private bool shooting = true;
 
-    // Start is called before the first frame update
-    void Start()
+    void Update()
     {
+        if(shooting && Input.GetMouseButton(0))
+        {
+            StartCoroutine(SpawnBullet());
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnBullet()
     {
-        if(Input.GetMouseButton(0))
-        {
-            Instantiate(bullet, shootPoint.position, shootPoint.rotation);
-        }
+        shooting = false;
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+        yield return new WaitForSeconds(0.086f);
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        shooting = true;  
     }
 }
