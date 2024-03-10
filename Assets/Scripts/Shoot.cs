81b855194bbe11f36115bcd61a2396542f0f900a
Sharkpb8 <sharkpb8@gmail.com>
2024-03-10 14:44:04 +0100

    shoot change to Raycast

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    public GameObject bullet;
-    public Transform shootPoint;
     private bool shooting = true;
+    Ray ray;
+    RaycastHit hit;
 
     void Update()
     {
+        ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * 20, Color.red);
+
         if(shooting && Input.GetMouseButton(0))
         {
-            StartCoroutine(SpawnBullet());
+            StartCoroutine(Fire());
         }
         
     }
 
-    IEnumerator SpawnBullet()
+    IEnumerator Fire()
     {
         shooting = false;
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
-        Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+         Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        if (Physics.Raycast(ray, out hit, 5))
+            {
+                
+            }
         yield return new WaitForSeconds(0.086f);
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
         shooting = true;  
