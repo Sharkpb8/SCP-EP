18a9eebecccf3f3880274919be0a30e69ac4cc48
Sharkpb8 <sharkpb8@gmail.com>
2024-04-03 18:17:21 +0200

    player keycard level

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Stats : MonoBehaviour
+{
+    public int Player_Level = 0;
+    public TextMeshProUGUI Level;
+
+    public int Returnplevel(){
+        return Player_Level;
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.E)){
+            IncreaseLevel();
+        }
+        if(Input.GetKeyDown(KeyCode.Q)){
+            DecreaseLevel();
+        }
+        Level.text = Player_Level.ToString();
+    }
+    public void IncreaseLevel(){
+        Player_Level++;
+        if(Player_Level>5){
+            Player_Level = 5;
+        }
+    }
+
+    public void DecreaseLevel(){
+        Player_Level--;
+        if(Player_Level<0){
+            Player_Level = 0;
+        }
+    }
+
+}
