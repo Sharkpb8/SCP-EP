ddb1356d38d486147686a61cb8dd7236c3fe3834
Sharkpb8 <sharkpb8@gmail.com>
2024-04-07 10:51:26 +0200

    not working text for keycard

--- Assets/Scripts/KeyCard.cs
+++ Assets/Scripts/KeyCard.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class KeyCard : MonoBehaviour
 {
     public GameObject Color_part;
     public int Level;
+    public GameObject Player;
+    public TextMeshProUGUI InteractText;
     public Material[] levelMaterials;
     private int colorindex;
     void Start()
@@ -13,11 +17,24 @@ public class KeyCard : MonoBehaviour
         Renderer ColorRender = Color_part.GetComponent<Renderer>();
         colorindex=Level-1;
         ColorRender.material  = levelMaterials[colorindex];
+        InteractText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(Player.transform.position, transform.position);
+        if(distance < 3f){
+            enable(true);
+            if(Input.GetKeyDown(KeyCode.F)){
+                
+            }
+        }else{
+            enable(false);
+        }
+    }
+
+    private void enable(bool enable){
+        InteractText.gameObject.SetActive(enable);
     }
 }
