8f32fb2ab1fdb9d629f3f37f40997fec54bbbe8d
Sharkpb8 <sharkpb8@gmail.com>
2024-04-10 19:41:25 +0200

    SCP-49 patroling with bodies

--- Assets/Scripts/SCP_049.cs
+++ Assets/Scripts/SCP_049.cs
@@ -6,30 +6,55 @@ using UnityEngine.AI;
 public class SCP_049 : MonoBehaviour
 {
     public Transform []PatrolPoints;
+    public Transform []SpawnPoints;
     public GameObject Player;
+    public GameObject SCP_49_2;
     public  NavMeshAgent agent;
     private int currentWaypoint = 0;
+    private int body_spawnpoints = 0;
+    private GameObject []bodies;
+    private Transform target;
     void Start()
     {
-        
+        bodies = GameObject.FindGameObjectsWithTag("Body");
     }
 
     // Update is called once per frame
     void Update()
     {
+        FindNearestBody();
         float distanceToPlayer = Vector3.Distance(Player.transform.position, transform.position);
-        /* float distanceToWaypoint = Vector3.Distance(transform.position, PatrolPoints[currentWaypoint].position); */
+        float distanceToBody = Vector3.Distance(target.position,transform.position);
+        float distanceTopatrol = Vector3.Distance(PatrolPoints[currentWaypoint].position,transform.position);
         if(distanceToPlayer<10f){
             agent.SetDestination(Player.transform.position);
 
         }else{
-            agent.SetDestination(PatrolPoints[currentWaypoint].position);
+            if(distanceToBody<distanceTopatrol){
+                agent.SetDestination(target.position);
+            }else{
+                agent.SetDestination(PatrolPoints[currentWaypoint].position);
                 currentWaypoint++;
                 if (currentWaypoint == PatrolPoints.Length)
                 {
                     currentWaypoint = 0;
                 }
+            }
         }
         
+    }
+
+    private void FindNearestBody()
+    {
+        float minDistance = Mathf.Infinity;
+        foreach (GameObject b in bodies)
+        {
+            float distance = Vector3.Distance(transform.position, b.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                target = b.transform;
+            }
+        }
     }
 }
