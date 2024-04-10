94ce2ea6a6c03a5a841136fe8e17fc0cd06a1e3b
Sharkpb8 <sharkpb8@gmail.com>
2024-04-10 21:18:34 +0200

    SCP-49-2 spawns but its armagedon

--- Assets/Scripts/SCP_049.cs
+++ Assets/Scripts/SCP_049.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.PlayerLoop;
 
 public class SCP_049 : MonoBehaviour
 {
     public Transform []PatrolPoints;
-    public Transform []SpawnPoints;
+    public Transform SpawnPoints;
     public GameObject Player;
     public GameObject SCP_49_2;
     public  NavMeshAgent agent;
     private int currentWaypoint = 0;
+    private int SCP_049_2_count = 0;
 
     // Update is called once per frame
     void Update()
@@ -19,6 +21,7 @@ public class SCP_049 : MonoBehaviour
         float distanceTopatrol = Vector3.Distance(PatrolPoints[currentWaypoint].position,transform.position);
         if(distanceToPlayer<10f){
             agent.SetDestination(Player.transform.position);
+            Instantiate(SCP_49_2,SpawnPoints);
 
         }else{
             agent.SetDestination(PatrolPoints[currentWaypoint].position);
