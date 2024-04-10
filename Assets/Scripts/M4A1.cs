5f7943182c76a6018e0ff206222be592f4569e79
Sharkpb8 <sharkpb8@gmail.com>
2024-04-10 15:48:32 +0200

    uzi script + fix of bool name

--- Assets/Scripts/M4A1.cs
+++ Assets/Scripts/M4A1.cs
@@ -10,7 +10,7 @@ public class M4A1 : MonoBehaviour
     public GameObject bullethole;
     public int MagazineCap = 30;
     public TextMeshProUGUI mag;
-    private bool M4A1ing = true;
+    private bool Shooting = true;
     Ray ray;
     RaycastHit hit;
 
@@ -19,7 +19,7 @@ public class M4A1 : MonoBehaviour
         ray = new Ray(transform.position, transform.forward);
         Debug.DrawRay(ray.origin, ray.direction * 20, Color.red);
 
-        if(M4A1ing && Input.GetMouseButton(0) && MagazineCap > 0)
+        if(Shooting && Input.GetMouseButton(0) && MagazineCap > 0)
         {
             StartCoroutine(Fire());
             MagazineCap -= 1;
@@ -36,7 +36,7 @@ public class M4A1 : MonoBehaviour
 
     IEnumerator Fire()
     {
-        M4A1ing = false;
+        Shooting = false;
          Debug.Log("Started Coroutine at timestamp : " + Time.time);
         if (Physics.Raycast(ray, out hit, 500))
             {
@@ -44,6 +44,6 @@ public class M4A1 : MonoBehaviour
             }
         yield return new WaitForSeconds(0.086f);
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
-        M4A1ing = true;  
+        Shooting = true;  
     }
 }
--- Assets/Scripts/Uzi.cs
+++ Assets/Scripts/Uzi.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Uzi : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject bullethole;
+    public int MagazineCap = 40;
+    public TextMeshProUGUI mag;
+    private bool Shooting = true;
+    Ray ray;
+    RaycastHit hit;
+
+    void Update()
     {
+        ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * 20, Color.red);
+
+        if(Shooting && Input.GetMouseButton(0) && MagazineCap > 0)
+        {
+            StartCoroutine(Fire());
+            MagazineCap -= 1;
+            mag.text = MagazineCap+"/40";
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            MagazineCap = 40;
+            mag.text = MagazineCap+"/40";
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Fire()
     {
-        
+        Shooting = false;
+         Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        if (Physics.Raycast(ray, out hit, 500))
+            {
+                Instantiate(bullethole,hit.point + (hit.normal *0.1f),Quaternion.FromToRotation(Vector3.up,hit.normal));
+            }
+        yield return new WaitForSeconds(0.010f);
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        Shooting = true;  
     }
 }
