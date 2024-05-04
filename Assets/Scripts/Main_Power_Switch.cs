f47f85d0d8eaf80d3830d73ab3578c6a37802441
Sharkpb8 <sharkpb8@gmail.com>
2024-05-04 15:26:30 +0200

    main switch not working lights

--- Assets/Scripts/Main_Power_Switch.cs
+++ Assets/Scripts/Main_Power_Switch.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Main_Power_Switch : MonoBehaviour
 {
     public Material powered_color;
+    public GameObject[] Lights;
+    public GameObject Switch;
     private int generatorspowered = 0;
+    private int lightindex = 0;
+    private Renderer lightrenderer;
     void Start()
     {
-        
+        Debug.Log("lenght start"+Lights.Length);
     }
 
     // Update is called once per frame
@@ -23,5 +27,12 @@ public class Main_Power_Switch : MonoBehaviour
     public void power()
     {
         generatorspowered++;
+        Debug.Log("1."+lightindex);
+        Debug.Log("lenght"+Lights.Length);
+        lightrenderer = Lights[lightindex].GetComponent<Renderer>();
+        Debug.Log("2."+lightindex);
+        lightindex++;
+        Debug.Log("3."+lightindex);
+        lightrenderer.material = powered_color;
     }
 }
