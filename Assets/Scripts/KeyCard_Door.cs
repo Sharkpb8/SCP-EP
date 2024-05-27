6f7d9d7748ace4b2a6b5cd77eaa834030bf3defd
Sharkpb8 <sharkpb8@gmail.com>
2024-05-27 16:25:33 +0200

    working script for new door model

--- Assets/Scripts/KeyCard_Door.cs
+++ Assets/Scripts/KeyCard_Door.cs
@@ -10,59 +10,44 @@ using Unity.AI.Navigation;
 
 public class KeyCard_Door : MonoBehaviour
 {
+    public GameObject Player;
     public GameObject Button;
-    public GameObject Door;
-    public GameObject Frame;
     public int Level;
     public Animator mAnimator;
     public Material[] levelMaterials;
     private int colorindex;
-    private bool DoorEnabled = true;
-    private Collider doorCollider;
-    private Collider frameCollider;
+    private bool DoorOpen = true;
     private Stats stats;
     void Start()
     {
-        GameObject statsObject = GameObject.Find("Capsule");
-        stats = statsObject.GetComponent<Stats>();
+        stats = Player.GetComponent<Stats>();
         colorindex=Level-1;
         Renderer buttonRenderer = Button.GetComponent<Renderer>();
         buttonRenderer.material  = levelMaterials[colorindex];
-        doorCollider = Door.GetComponent<Collider>();
-        frameCollider = Frame.GetComponent<Collider>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(Camera.main.transform.position, Door.transform.position);
+        float distance = Vector3.Distance(Player.transform.position, transform.position);
         if(distance < 5f && checkLevel() && Input.GetKeyDown(KeyCode.F)){
-            ToggleDoor();
-            rebuild();
+            DoorOpen = !DoorOpen;
+            if(DoorOpen)
+            {
+                mAnimator.SetTrigger("TrClose");
+            }else{
+                mAnimator.SetTrigger("TrOpen");
+            }
         }
-    }
-    void ToggleDoor()
-    {
-        DoorEnabled = !DoorEnabled;
-        doorCollider.enabled = DoorEnabled;
-        frameCollider.enabled = DoorEnabled;
-        
-        Door.SetActive(DoorEnabled);
     }
 
     private bool checkLevel(){
-        if(stats.Player_Level>=Level){
+        if(stats.Player_Level>=Level)
+        {
             return true;
         }else{
             return false;
         }
-    }
-
-    private void rebuild()
-    {
-        GameObject NavRebuild = GameObject.Find("NavMesh Rebuilder");
-        NavMesh_ReBuilder nmb = NavRebuild.GetComponent<NavMesh_ReBuilder>();
-        nmb.RebuildMesh();
     }
 }
