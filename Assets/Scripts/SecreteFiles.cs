573ad454de185afcc7c6f9e5920668b304e3e8f3
Sharkpb8 <sharkpb8@gmail.com>
2024-06-05 16:17:09 +0200

    files added

---
+++ Assets/Scripts/SecreteFiles.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SecreteFiles : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(Player.transform.position, transform.position);
+        if(distance < 5f && Input.GetKeyDown(KeyCode.F))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+        
+    }
+}
